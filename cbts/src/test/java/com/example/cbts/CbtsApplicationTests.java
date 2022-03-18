fcb52c2f59f33a0577584d56b205c80dc9d3b58b
newtepos <supakit.pnm@gmail.com>
2022-03-18 15:54:55 +0700

    #1: Create CBTS & BBS Project

---
+++ cbts/src/test/java/com/example/cbts/CbtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cbts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
