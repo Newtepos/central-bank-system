b04aeed84bbe3deb62c5979bfb2a4b7892dff7b5
newtepos <supakit.pnm@gmail.com>
2022-03-18 16:49:46 +0700

    #6: Created createBank controller api

--- cbts/src/main/java/com/example/cbts/controller/CBTSController.java
+++ cbts/src/main/java/com/example/cbts/controller/CBTSController.java
@@ -1,7 +1,12 @@
 package com.example.cbts.controller;
 
+import com.example.cbts.request.BankRequest;
 import com.example.cbts.service.CBTSService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +15,10 @@ public class CBTSController {
     @Autowired
     CBTSService cbtsService;
 
-    public void createBank() {
+    @PostMapping("/bank")
+    public ResponseEntity createBank(@RequestBody BankRequest bankRequest) {
+        cbtsService.createBank(bankRequest);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
