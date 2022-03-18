7139fbfb25ebce32d1e1fb62741a964433fcbf8f
newtepos <supakit.pnm@gmail.com>
2022-03-18 16:22:33 +0700

    #3: Created MoneyTruckRepository

---
+++ cbts/src/main/java/com/example/cbts/repository/MoneyTruckRepository.java
@@ -0,0 +1,7 @@
+package com.example.cbts.repository;
+
+import com.example.cbts.entites.MoneyTruck;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoneyTruckRepository extends JpaRepository<MoneyTruck, Long> {
+}
