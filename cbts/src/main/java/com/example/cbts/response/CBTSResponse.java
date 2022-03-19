b03ddb712b6acbcbf94c1ade052cb8648ea48bf7
newtepos <supakit.pnm@gmail.com>
2022-03-19 17:59:36 +0700

    #9: Created CBTSResponse

---
+++ cbts/src/main/java/com/example/cbts/response/CBTSResponse.java
@@ -0,0 +1,27 @@
+package com.example.cbts.response;
+
+public class CBTSResponse {
+    private long code;
+    private String message;
+
+    public CBTSResponse(long code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
