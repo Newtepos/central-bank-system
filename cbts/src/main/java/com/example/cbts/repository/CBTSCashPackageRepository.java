822cd16d47f2e290472b4cfaee0c10e3fd3cda4c
newtepos <supakit.pnm@gmail.com>
2022-03-19 21:26:46 +0700

    #7: Created CBTSCashPackageRepository

---
+++ cbts/src/main/java/com/example/cbts/repository/CBTSCashPackageRepository.java
@@ -0,0 +1,7 @@
+package com.example.cbts.repository;
+
+import com.example.cbts.entites.CBTSCashPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CBTSCashPackageRepository extends JpaRepository<CBTSCashPackage, Long> {
+}
