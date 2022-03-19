0a416226a0b41803660c8127f680fbc13cedbe12
newtepos <supakit.pnm@gmail.com>
2022-03-19 15:23:46 +0700

    #6: Updated CurrencyRepository to have findbyCurrency Fuc.

--- cbts/src/main/java/com/example/cbts/repository/CurrencyRepository.java
+++ cbts/src/main/java/com/example/cbts/repository/CurrencyRepository.java
@@ -2,6 +2,11 @@ package com.example.cbts.repository;
 
 import com.example.cbts.entites.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
+     Optional<Currency> findByCurrency(String currency);
 }
